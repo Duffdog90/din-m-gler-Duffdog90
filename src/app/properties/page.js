05db8f85d5fa99861b234039c9793cea55672b27
Duffdog90 <soden17@gmail.com>
2025-01-20 08:17:06 +0100

    fixes

--- src/app/properties/page.js
+++ src/app/properties/page.js
@@ -114,7 +114,7 @@ export default function Properties() {
                     {visibleCount < homes.length && (
                         <button 
                             onClick={loadMore} 
-                            className="mt-6 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+                            className="mt-6 px-6 py-3 bg-[#162A41] text-white rounded-lg hover:bg-[#162A41]"
                         >
                             Vis flere
                         </button>
